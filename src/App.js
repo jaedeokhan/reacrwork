0f8727ddb8a0ee0fa62ae5e6e23c5e59254406d9
jaedeokhan <hjaedeok@gmail.com>
2022-12-31 16:57:29 +0900

    import 관련 내용 - import { useEffect } from 'react';    - import 구문에서 중괄호({})로 감싸는 이유는 function이나 Object가      아니고, 변수를 보내주는 방식이다.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import Sub from './Sub';
+import Third from './Third';
+import { num } from './Sub';
 
 function App() {
-  console.log('App 실행됨');
-
-  let [num, setNum] = useState(5);
-
-  let sample = [
-    { id: 1, name: '홍길동' },
-    { id: 2, name: '임꺽정' },
-    { id: 3, name: '장보고' },
-    { id: 4, name: '이순신' },
-  ];
-
-  // 다운로드 받음
-  const [users, setUsers] = useState(sample);
-
-  const download = () => {
-    setUsers([...sample, { id: num, name: '조자룡' }]);
-    setNum(num + 1);
-  };
-
-  // 랜더링 시점 = 상태값 변경
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((user) => (
-        <h1 key={user.id}>
-          {user.id}, {user.name}
-        </h1>
-      ))}
+      <Sub />
+      <Third />
+      {num}
+      Hello World!
     </div>
   );
 }
--- src/Sub.js
+++ src/Sub.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
+let num = 10;
+
 const Sub = () => {
-
-    // 부모 태그가 변경이 되면 연관되어 있는 자식 태그들 전체를 랜더링한다.
-    // 불필요한 랜더링이 생기면 성능에 영향을 끼친다.
-    // 성능 해결이 가능한 함수 제공!
-
-    return (
-    <div>
-        <h1>Sub입니다.</h1>
-    </div>
-    );
+  return <div></div>;
 };
 
+export { num }; // 이게 뭐꼬?
 export default Sub;
