bb78f867390cdcb5edddee05c4073e99b3a35e8a
jaedeokhan <hjaedeok@gmail.com>
2023-01-01 19:12:30 +0900

    useMemo - 메모라이제이션! - 같은 컴포넌트안에 하나의 컴포넌트만 변경하고 싶을 때 사용! - <div>{firstEle} {SecondEle}</div>

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import { useEffect, useState } from 'react'; // {}로 있으면 export default가 아니라는 것이다.
+import { useState, useMemo } from 'react';
 
 function App() {
-  const [data, setData] = useState(0);
-  const [search, setSearch] = useState(0);
+  const [list, setList] = useState([1, 2, 3, 4]);
+  const [str, setStr] = useState('합계');
 
-  const download = () => {
-    // 다운로드 받고(통신)
-    let downloadData = 5;
-    setData(downloadData);
+  const getAddResult = () => {
+    let sum = 0;
+    list.forEach((i) => (sum += i));
+    console.log(sum);
+    return sum;
   };
 
-  // 함수 실행 시점은?
-  // (1) : App() 함수가 최초 실행될 때(App(0 그림이 최초 그려질 때 )) => (마운트될 때)
-  // (2) : 상태 변수가 변경될 때 (그게 dependencyList에 등록되어 있어야함)
-  // (3) : 의존리스트 관리를 할 수 있다
-  useEffect(() => {
-    console.log('useEffect 실행됨');
-    download();
-  }, [search]); // 두 번째 인자 : 어디에 의존할 건지? 비어두면 onLoad처럼 처음에만 호출이 된다.
+  const addResult = useMemo(() => getAddResult(), [list]);
 
   return (
     <div>
       <button
         onClick={() => {
-          setSearch(2);
+          setStr('안녕');
         }}
       >
-        검색하기
+        문자 변경
       </button>
-      <h1>데이터 : {data}</h1>
       <button
         onClick={() => {
-          setData(data + 1);
+          setList([...list, 10]);
         }}
       >
-        더하기
+        리스트값 추가
       </button>
+      <div>
+        {list.map((l) => (
+          <h1 key={l}>{l}</h1>
+        ))}
+      </div>
+      <div>
+        {str} : {addResult}
+      </div>
     </div>
   );
 }
